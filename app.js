3a310968c319243ef8a743c8b9973b2ea3906de5
humbatovqara <qarahumbtov@gmail.com>
2021-08-21 15:48:34 +0500

    First version

---
+++ app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const changeBtn = document.getElementById("changeBtn");
+const colorName = document.querySelector(".colorName");
+
+changeBtn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    colorName.textContent = colors[randomNumber];
+});
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * colors.length);
+}
